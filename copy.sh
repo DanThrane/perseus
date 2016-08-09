cecb3d48ecafdf3f5e67e3e11fcaf92abadfc7cc
= <dthrane@gmail.com>
2016-08-09 10:31:12 +0200

    Improves the copy script slightly

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
-cp build/test-renderer.js /home/dan/projects/tekvideo.sdu.dk/grails-app/assets/javascripts/perseus/
-cp build/editor-perseus.js /home/dan/projects/tekvideo.sdu.dk/grails-app/assets/javascripts/perseus/
-cp build/frame-perseus.js /home/dan/projects/tekvideo.sdu.dk/grails-app/assets/javascripts/perseus/
-cp build/demo-perseus.js /home/dan/projects/tekvideo.sdu.dk/grails-app/assets/javascripts/perseus/
+cp build/test-renderer.js "$TEKVIDEO_HOME/grails-app/assets/javascripts/perseus/"
+cp build/editor-perseus.js "$TEKVIDEO_HOME/grails-app/assets/javascripts/perseus/"
+cp build/frame-perseus.js "$TEKVIDEO_HOME/grails-app/assets/javascripts/perseus/"
+cp build/demo-perseus.js "$TEKVIDEO_HOME/grails-app/assets/javascripts/perseus/"
 echo "Copied"
